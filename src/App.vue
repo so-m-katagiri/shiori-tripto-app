ccbdf3de42e8c59f81d16fe17f7d3c35bf2cacea
so <43242083+so-m-katagiri@users.noreply.github.com>
2018-10-04 02:37:11 +0900

    add bulma

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld/>
+    <Main/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Main from './components/Main'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Main
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 
+// Require the main Sass manifest file
+require('./assets/sass/main.scss')
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
